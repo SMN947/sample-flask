cf551d7e43b34ab5ada5ef9db782a184284f48b7
SMN947 <betum98@gmail.com>
2023-10-06 11:18:53 -0500

    feat: Added sockets test

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'  # Replace with your secret key
+socketio = SocketIO(app)
 
+# Initialize a variable to keep track of connected clients
+connected_clients = 0
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html")
+def index():
+    return render_template("index.html", connected_clients=connected_clients)
+
+@socketio.on('connect')
+def handle_connect():
+    global connected_clients
+    connected_clients += 1
+    emit('update_clients_count', connected_clients, broadcast=True)
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    global connected_clients
+    connected_clients -= 1
+    emit('update_clients_count', connected_clients, broadcast=True)
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
